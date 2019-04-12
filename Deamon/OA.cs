0c1c3ea20310799aec338c5819652d4ed3ead06d
zhanghy97 <1744008817@qq.com>
2019-04-12 17:47:04 +0800

    first commit Deamon v1.0

---
+++ Deamon/OA.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Deamon
+{
+    public class OA
+    {
+
+        public string WPostGB2312(string oaserver, NameValueCollection values)
+        {
+            byte[] result = UploadFilesGB2312(oaserver, values);
+            return Encoding.GetEncoding("GB2312").GetString(result);
+        }
+
+        public static byte[] UploadFilesGB2312(string address, NameValueCollection values)
+        {
+            var request = WebRequest.Create(address);
+            request.Method = "POST";
+            var boundary = "-------------------------" + DateTime.Now.Ticks.ToString("x", NumberFormatInfo.InvariantInfo);
+            request.ContentType = "multipart/form-data; boundary=" + boundary;
+            boundary = "--" + boundary;
+
+            using (var requestStream = request.GetRequestStream())
+            {
+                // Write the values
+                foreach (string name in values.Keys)
+                {
+                    var buffer = Encoding.ASCII.GetBytes(boundary + Environment.NewLine);
+                    requestStream.Write(buffer, 0, buffer.Length);
+                    buffer =
+                        Encoding.ASCII.GetBytes(string.Format("Content-Disposition: form-data; name=\"{0}\"{1}{1}", name,
+                            Environment.NewLine));
+                    requestStream.Write(buffer, 0, buffer.Length);
+                    buffer = Encoding.GetEncoding("GB2312").GetBytes(values[name] + Environment.NewLine);
+                    requestStream.Write(buffer, 0, buffer.Length);
+                }
+
+                var boundaryBuffer = Encoding.ASCII.GetBytes(boundary + "--");
+                requestStream.Write(boundaryBuffer, 0, boundaryBuffer.Length);
+            }
+
+            using (var response = request.GetResponse())
+            using (var responseStream = response.GetResponseStream())
+            using (var stream = new MemoryStream())
+            {
+                responseStream.CopyTo(stream);
+                return stream.ToArray();
+            }
+        }
+    }
+}
---
+++ Deamon/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Specialized;
+
+namespace Deamon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] processIdAry = Process.GetProcessesByName("Deamon");
+
+            if (processIdAry.Count() > 1)
+            {
+                for (int i = 1; i < processIdAry.Count(); i++)
+                {
+                    processIdAry[i].Kill();
+                }
+            }
+            else
+            {
+                Console.WriteLine("守护程序启动成功！");
+            }
+
+            Ccout();
+        }
+
+
+        public static void Ccout()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            var configuration = builder.Build();
+
+            string[] pathList = Regex.Split(configuration["Path"], ",", RegexOptions.IgnoreCase);
+
+            string[] emailList = Regex.Split(configuration["Email"], ",", RegexOptions.IgnoreCase);
+
+            string logPath = configuration["LogPath"];
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+                //获取所有进程内的进程名并加入列表
+                Process[] ps = Process.GetProcesses();
+                var pcNameList = new List<string>();
+                foreach (Process p in ps)
+                {
+                    pcNameList.Add(p.ProcessName);
+                }
+                foreach (string pcPath in pathList)
+                {
+                    string[] b = Regex.Split(pcPath, "/", RegexOptions.IgnoreCase);
+                    bool exists = ((IList)pcNameList).Contains(b[1]);
+                    if (exists)
+                    {
+                        Console.WriteLine(b[1] + "程序正常运行");
+                    }
+                    else
+                    {
+                        foreach (string email in emailList) {
+                            sendmail(b[1],email);
+                        }
+                        string path = logPath;
+                        FileStream fs = new FileStream(path, FileMode.Append);
+                        StreamWriter sw = new StreamWriter(fs);
+                        sw.WriteLine("进程停止时间：");
+                        sw.WriteLine(DateTime.Now.ToLocalTime().ToString());
+                        sw.Close();
+                        fs.Close();
+
+                        Console.WriteLine(b[1] + "进程中断，正在执行重启任务");
+                        //重启这个进程
+                        var decodeProcess = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = b[0],
+                                UseShellExecute = false,
+                                RedirectStandardInput = false,
+                                RedirectStandardOutput = false,
+                                RedirectStandardError = false,
+                                CreateNoWindow = true
+                            }
+                        };
+
+                        decodeProcess.Start();
+                        FileStream fs1 = new FileStream(path, FileMode.Append);
+                        StreamWriter sw1 = new StreamWriter(fs1);
+                        sw1.WriteLine("进程重启时间：");
+                        sw1.WriteLine(DateTime.Now.ToLocalTime().ToString());
+                        sw1.Close();
+                        fs1.Close();
+                        Console.WriteLine("重启成功");
+                    }
+                }
+            }
+        }
+        public static void sendmail(string pathName,string email)
+        {
+            var builder1 = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            var configuration1 = builder1.Build();            
+
+
+            string key = configuration1["Key"];
+            string url = configuration1["Ip:Port"];
+
+            var values = new NameValueCollection {
+                    { "userid", ""},
+                    { "password", ""},
+                    { "SendTo", email },
+                    { "WebSubject", "提醒" },
+                    { "Body", pathName+"进程已关闭"},
+                    { "hz","0"},
+                    { "MailSMS","1"},
+                    { "__Click",key}
+                    };
+
+            new OA().WPostGB2312(url, values);
+
+            
+        }
+    }
+}
